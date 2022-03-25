37a4afccf5bf91f51c2e2668dfeebe6431e5c389
David Ku <hola@davidku.dev>
2022-03-24 22:05:43 -0500

    se edita api para requerir token

--- src/api/index.js
+++ src/api/index.js
@@ -3,32 +3,37 @@ import axios from "axios";
 export const fetchTasks = (token) => {
   return axios.get("http://localhost:5000/api/v1/tasks/", {
     headers: {
-      Authorization: `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
+      Authorization: `Bearer ${token}`,
       "Content-Type": "application/x-www-form-urlencoded",
     },
   });
 };
 
-export const createTask = (title, description) => {
+export const createTask = (title, token) => {
   const URL = "http://localhost:5000/api/v1/tasks/";
   return axios.post(
     URL,
-    { title, description },
+    { title },
     {
       headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
+        Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
     }
   );
 };
 
-export const deleteTask = (id) => {
+export const deleteTask = (id, token) => {
   const URL = "http://localhost:5000/api/v1/tasks/" + id;
   return axios.delete(URL, {
     headers: {
-      Authorization: `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
+      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     },
   });
+};
+
+export const login = (email, password) => {
+  const URL = "http://localhost:5000/api/v1/users/login";
+  return axios.post(URL, { email, password });
 };
